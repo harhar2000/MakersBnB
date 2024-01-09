77316c036369b3560405a2967202cbe0d0034194
Katie <katieanthonisz@hotmail.co.uk>
2024-01-09 12:17:55 +0000

    Created space classes

---
+++ lib/space.py
@@ -0,0 +1,20 @@
+class Space:
+    # We initialise with all of our attributes
+    # Each column in the table should have an attribute here
+    def __init__(self, id, space_name, space_description, price, user_id):
+        self.id = id
+        self.space_name = space_name
+        self.space_description= space_description
+        self.price = price
+        self.user_id = user_id
+
+    # This method allows our tests to assert that the objects it expects
+    # are the objects we made based on the database records.
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+
+    # This method makes it look nicer when we print an Artist
+    def __repr__(self):
+        return f"Space({self.id}, {self.space_name}, {self.space_description}, {self.price}, {self.user_id})"
+
+   
---
+++ tests/test_space_repository.py
@@ -0,0 +1,36 @@
+from lib.space_repository import SpaceRepository
+from lib.space import Space
+
+"""
+When we call SpaceRepository#all
+We get a list of Space objects reflecting the seed data.
+"""
+def test_get_all_records(db_connection): # See conftest.py to learn what `db_connection` is.
+    # Seed our database with some test data
+    repository = SpaceRepository(db_connection) # Create a new SpaceRepository
+
+    spaces = repository.all() # Get all spaces
+
+    # Assert on the results
+    assert spaces == [
+        Space(1,'test space name', 'test space description', 1, 1),
+    ]
+
+
+"""
+When we call SpaceRepository#create
+We get a new record in the database.
+"""
+def test_create_record(db_connection):
+    repository = SpaceRepository(db_connection)
+
+    created_space = repository.create(Space(None, "another_space_name", "another_description", 2, 1))
+    assert created_space == Space(2, "another_space_name", "another_description", 2, 1)
+
+    result = repository.all()
+    assert result == [
+        Space(1,'test space name', 'test space description', 1, 1),
+        Space(2, "another_space_name", "another_description", 2, 1)
+    ]
+    
+
