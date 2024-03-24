1b7226841e74025cc69f76ae852890a60b6f6bea
Jehoshua <137894662+AVJehoshua@users.noreply.github.com>
2024-03-24 15:35:13 +0000

    minor changes

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 // index.js
-import React from 'react';
-import NavBar from './frontend/src/components/navBar.jsx'; // Import your React component
-import { createRoot } from 'react-dom-client'
+import React from "react";
+import NavBar from "./frontend/src/components/navBar.jsx"; // Import your React component
+import { createRoot } from "react-dom-client";
 
+const container = document.getElementById("navbar");
 
-const container = document.getElementById('navbar')
-
-const root = createRoot(container)
+const root = createRoot(container);
 root.render(<NavBar />);
-
